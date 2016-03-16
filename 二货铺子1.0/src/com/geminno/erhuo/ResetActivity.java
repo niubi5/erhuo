6376726f9af9559012e64976de492d3e2be82aa3
wenwenruya <282986708@qq.com>
2016-03-16 14:05:27 +0800

    重置密码

--- 二货铺子1.0/src/com/geminno/erhuo/ResetActivity.java
+++ 二货铺子1.0/src/com/geminno/erhuo/ResetActivity.java
@@ -8,29 +8,15 @@ import android.view.Window;
 
 public class ResetActivity extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_reset);
+		 //调用setColor()方法,实现沉浸式状态栏
+	  	MainActivity.setColor(this, getResources().getColor(R.color.main_red));
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.reset, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
---
+++ 二货铺子1.0/src/com/geminno/erhuo/ZhaoHuiActivity.java
@@ -0,0 +1,43 @@
+package com.geminno.erhuo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+
+public class ZhaoHuiActivity extends Activity implements OnClickListener{
+	
+	Button button;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_zhao_hui);
+		//调用setColor()方法,实现沉浸式状态栏
+	  	MainActivity.setColor(this, getResources().getColor(R.color.main_red));
+	    button=(Button) findViewById(R.id.btn_verify_zhaohui);
+	    button.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.btn_verify_zhaohui:
+			Intent intent = new Intent(this,ResetActivity.class);
+			startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	
+}
