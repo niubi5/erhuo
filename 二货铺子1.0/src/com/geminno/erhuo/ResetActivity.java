d0951d9cabd587ca762b16ebea80a021388df8d3
heikki1993 <916310989@qq.com>
2016-03-18 10:18:32 +0800

    10.18

--- 二货铺子1.0/src/com/geminno/erhuo/ResetActivity.java
+++ 二货铺子1.0/src/com/geminno/erhuo/ResetActivity.java
@@ -15,6 +15,7 @@ public class ResetActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_reset);
 		 //调用setColor()方法,实现沉浸式状态栏
+		//10.18heikki
 	  	MainActivity.setColor(this, getResources().getColor(R.color.login_background));
 	}
 	
