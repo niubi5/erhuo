987c5a6d4052979e0c8bded9d5f08546a9410cfd
heikki1993 <916310989@qq.com>
2016-03-18 10:53:31 +0800

    10:53

--- 二货铺子1.0/src/com/geminno/erhuo/view/AddImageView.java
+++ 二货铺子1.0/src/com/geminno/erhuo/view/AddImageView.java
@@ -12,7 +12,7 @@ public class AddImageView extends FrameLayout {
 	public AddImageView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		//huagnjie 10:46
-		LayoutInflater.from(context).inflate(1, this);
+		LayoutInflater.from(context).inflate(2, this);
 	}
 
 }
