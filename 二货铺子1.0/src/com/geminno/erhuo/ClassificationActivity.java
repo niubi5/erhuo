205ed71bd71fd163daa578396e72db0e9f857f69
luoshiheng <577487870@qq.com>
2016-03-28 11:50:49 +0800

    12

--- 二货铺子1.0/src/com/geminno/erhuo/ClassificationActivity.java
+++ 二货铺子1.0/src/com/geminno/erhuo/ClassificationActivity.java
@@ -28,9 +28,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -40,15 +43,13 @@ public class ClassificationActivity extends Activity implements OnClickListener 
 	private Context context;
 	private Spinner classificationSpinner = null; // 分类
 	private Spinner sortSpinner = null; // 排序
-	private Spinner screenSpinner = null; // 筛选
 	ArrayAdapter<String> classificationAdapter = null; // 分类适配器
 	ArrayAdapter<String> sortAdapter = null; // 排序适配器
 	ArrayAdapter<String> screenAdapter = null; // 筛选适配器
 	private RefreshListView refreshListView;
 	private SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");
 	// spinner中的分类
-	private String[] classif = new String[] { "分类", "全部分类", "手机", "数码", "交通工具",
-			"生活文体", "二手书苑", "美妆美饰", "鞋服箱包", "其他", };
+	private String[] classif;
 	private String type = null;
 	private String tag = null;
 	private HomePageAdapter adapter;
@@ -120,38 +121,33 @@ public class ClassificationActivity extends Activity implements OnClickListener 
 	}
 
 	private void setSpinner() {
-		if (type.equals("苹果手机")) {
-			classif = new String[] { type, "全部分类", "平板电脑", "笔记本", "小米",
-					"数码3c", "书籍文体", "美容美体", "箱包", "其他", };
-		} else if (type.equals("平板电脑")) {
-			classif = new String[] { type, "全部分类", "苹果手机", "笔记本", "小米",
-					"数码3c", "书籍文体", "美容美体", "箱包", "其他", };
-		} else if (type.equals("笔记本")) {
-			classif = new String[] { type, "全部分类", "苹果手机", "平板电脑", "小米",
-					"数码3c", "书籍文体", "美容美体", "箱包", "其他", };
-		} else if (type.equals("小米")) {
-			classif = new String[] { type, "全部分类", "苹果手机", "平板电脑", "笔记本",
-					"数码3c", "书籍文体", "美容美体", "箱包", "其他", };
-		} else if (type.equals("数码3C")) {
-			classif = new String[] { type, "全部分类", "苹果手机", "平板电脑", "笔记本",
-					"小米", "书籍文体", "美容美体", "箱包", "其他", };
-		} else if (type.equals("书籍文体")) {
-			classif = new String[] { type, "全部分类", "苹果手机", "平板电脑", "笔记本",
-					"小米", "数码3c", "美容美体", "箱包", "其他", };
-		} else if (type.equals("服装鞋包")) {
-			classif = new String[] { type, "全部分类", "苹果手机", "平板电脑", "笔记本",
-					"小米", "数码3c", "美容美体", "书籍文体", "其他", };
-		} else if (type.equals("美容美体")) {
-			classif = new String[] { type, "全部分类", "苹果手机", "平板电脑", "笔记本",
-					"小米", "数码3c", "书籍文体", "箱包", "其他", };
-		}
+		classif = new String[] { type, "苹果手机", "平板电脑", "笔记本", "小米", "数码3c",
+				"书籍文体", "美容美体", "服装箱包", "其他", };
 		String[] sort = new String[] { "排序", "默认排序", "最新发布", "离我最近", "价格最低",
 				"价格最高" };
-		String[] screen = new String[] { "选择价格", "0~500元", "500~1000元",
-				"1000~1500元", "1500~2000元", "2000~2500元", "2500以上" };
 		classificationSpinner = (Spinner) findViewById(R.id.spinner1);
 		sortSpinner = (Spinner) findViewById(R.id.spinner2);
-		screenSpinner = (Spinner) findViewById(R.id.spinner3);
+		classificationSpinner
+				.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+					@Override
+					public void onItemSelected(AdapterView<?> parent,
+							View view, int position, long id) {
+						// 点第一个不做处理
+						Log.i("erhuo", "item点击事件");
+						if (position != 0) {
+							tag = position + "";
+							listAll.clear();
+							curPage = 1;
+							initData();
+						}
+					}
+
+					@Override
+					public void onNothingSelected(AdapterView<?> parent) {
+
+					}
+				});
 		// 绑定适配器
 		classificationAdapter = new ArrayAdapter<String>(this,
 				R.layout.spinner_search, classif);
@@ -168,19 +164,10 @@ public class ClassificationActivity extends Activity implements OnClickListener 
 				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// 默认第一个
 		sortSpinner.setSelection(0, true);
-		// 绑定适配器
-		screenAdapter = new ArrayAdapter<String>(this, R.layout.spinner_search,
-				screen);
-		screenSpinner.setAdapter(screenAdapter);
-		screenAdapter
-				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		// 默认第一个
-		screenSpinner.setSelection(0, true);
 	}
 
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case R.id.ib_fenlei:
 			// 返回
@@ -204,7 +191,6 @@ public class ClassificationActivity extends Activity implements OnClickListener 
 
 					@Override
 					public void onFailure(HttpException arg0, String arg1) {
-						// TODO Auto-generated method stub
 
 					}
 
@@ -290,6 +276,7 @@ public class ClassificationActivity extends Activity implements OnClickListener 
 										refreshListView, isRefersh);
 								refreshListView.setAdapter(adapter);
 							} else {
+								Log.i("erhuo", "通知数据源改变");
 								adapter.notifyDataSetChanged();
 							}
 						}
